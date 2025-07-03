e42f09bafd2123640fa841f2af5cbe9fc258921a
haingchin <haingchin@gmail.com>
2025-07-03 08:17:53 +0630

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function index()
+    {   
+        $products = Product::all();
+        return Inertia::render('Products/Index', compact('products'));
+    }
+
+    public function create()
+    {
+        return Inertia::render('Products/Create', []);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=> 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'description' => 'nullable|string',
+        ]);
+
+        Product::create($request->all());
+        return redirect()->route('products.index')->with('success', 'Product created successfully.');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
+    }
+}
---
+++ resources/js/pages/Products/Index.tsx
@@ -0,0 +1,96 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link, useForm, usePage } from '@inertiajs/react';
+import { Megaphone } from 'lucide-react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+interface PageProps {
+    flash: {
+        success?: String;
+    };
+    products: Product[];
+}
+
+export default function index() {
+    const { products, flash } = usePage().props as PageProps;
+
+    const { processing, delete: destroy } = useForm();
+
+    const handleDelete = (id: number, name: string) => {
+        if (confirm(`Do you want to delete a product - ${id}. ${name}`)) {
+            destroy(route('products.destroy', id));
+        }
+    };
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Products" />
+            <div className="m-4">
+                <div>
+                    {flash.success && (
+                        <Alert>
+                            <Megaphone className="h-4 w-4" />
+                            <AlertTitle>Notification!</AlertTitle>
+                            <AlertDescription>{flash.success}</AlertDescription>
+                        </Alert>
+                    )}
+                </div>
+            </div>
+            <div className="m-4">
+                <Link href={route('products.create')}>
+                    <Button>Create a Product</Button>
+                </Link>
+            </div>
+            {products.length >> 0 && (
+                <div className="m-4">
+                    <Table>
+                        <TableCaption>A list of your recent products.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">ID</TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Price</TableHead>
+                                <TableHead>Description</TableHead>
+                                <TableHead className="text-center">Action</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {products.map((product) => (
+                                <TableRow>
+                                    <TableCell className="font-medium">{product.id}</TableCell>
+                                    <TableCell>{product.name}</TableCell>
+                                    <TableCell>{product.price}</TableCell>
+                                    <TableCell>{product.description}</TableCell>
+                                    <TableCell className="text-center">
+                                        <Button
+                                            disabled={processing}
+                                            onClick={() => handleDelete(product.id, product.name)}
+                                            className="bg-red-400 hover:bg-red-700"
+                                        >
+                                            Delete
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
+        </AppLayout>
+    );
+}
